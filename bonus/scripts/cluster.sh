40d838c2f90e7afa093939d1ce0dc99d09fac936
jessica <jessica.l59@outlook.fr>
2026-01-13 15:56:01 +0100

    bonus

--- bonus/scripts/cluster.sh
+++ bonus/scripts/cluster.sh
@@ -1,17 +1,9 @@
 #!/usr/bin/env bash
-k3d cluster create inception-of-things -p "8888:30080@loadbalancer"
-
-kubectl create namespace argocd || true
 kubectl create namespace gitlab || true
 
-# Delete existing installations if any
-kubectl delete -n argocd all --all || true
-# Install ArgoCD
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
 # Install GitLab
 kubectl apply -n gitlab -f ../confs/gitlab.yaml
 
-# Port-forward ArgoCD server
-kubectl port-forward -n argocd svc/argocd-server 8080:80 /dev/null 2>&1 &
 # Port-forward GitLab webservice
-kubectl port-forward -n gitlab svc/gitlab-webservice-default 8181:80 /dev/null 2>&1 &
+kubectl port-forward -n gitlab gitlab 8181:80
--- p3/scripts/install.sh
+++ p3/scripts/install.sh
@@ -16,7 +16,7 @@ sudo chmod a+r /etc/apt/keyrings/docker.asc
 # Add the repository to Apt sources:
 sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
 Types: deb
-URIs: https://download.docker.com/linux/ubuntu
+URIs: https://download.docker.com/linux/debian
 Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
 Components: stable
 Signed-By: /etc/apt/keyrings/docker.asc
